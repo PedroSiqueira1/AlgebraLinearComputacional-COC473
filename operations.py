a26ee581aad5aa010f7c6a1c63e2511f5dbf07b3
ptk-trindade <PatrickTrindade@poli.ufrj.br>
2022-05-01 14:52:03 -0300

    forward_substitution usando o numpy
    usando o np dentro da forward_substitution para gerar o vetor y (com 0s)

--- operations.py
+++ operations.py
@@ -1,19 +1,20 @@
-﻿def forward_substitution(matrix_l, vector_b):
-    y = []
+﻿import numpy as np
+
+def forward_substitution(matrix_l, vector_b):
+    y = np.zeros(len(vector_b), float) 
 
     for i in range(len(vector_b)):
         summ = 0
         for j in range(i):
             summ += matrix_l[i][j] * y[j]
          
-        y.append((vector_b[i] - summ)) #/matrix_l[i][i])
+        y[i] = (vector_b[i] - summ) #/matrix_l[i][i])
 
     return y
 
 def backward_substitution():
 
     return 
-
 
 
 def pivot(matrix_a, line):
