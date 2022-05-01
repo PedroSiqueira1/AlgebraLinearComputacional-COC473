364b9780a6d0f55478a5ca054aeb0b2efdd403ed
ptk-trindade <PatrickTrindade@poli.ufrj.br>
2022-05-01 12:03:58 -0300

    Criada a classe Matrix
    Criei a classe "Matrix" mas ela ainda não foi testada

--- Trabalho1.py
+++ Trabalho1.py
@@ -1,10 +1,63 @@
 import numpy as np
 
+class Matrix():
+    matrix = None
+    scope = None    # 'A', 'L' or 'U'
+    size = (None, None)
+
+    def __init__(self, matrix, scope='A'):
+        def valid(matrix, scope): # verifies if the created matrix is valid
+            # SCOPE
+            if scope not in {'A', 'L', 'U'}:
+                print(f"{scope} is an invalid scope, it should be: A, L or U")
+                return False
+
+            # MATRIX - Not empty + all lines must have the same width
+            length = len(matrix)
+            
+            if (size == 0 or len(matrix[0]) == 0):
+                print("Empty matrix")
+                return False
+            
+            width = len(matrix[0])
+            for c in range(1, length):
+                if len(matrix[c]) != width:
+                    print(f"Line {c} has {len(matrix[c])}, when it should have {width}")
+                    return False
+
+            return True
+
+
+        if not valid(matrix, scope):
+            print("ERROR - Invalid matrix")
+
+        self.scope = scope
+        self.matrix = matrix
+        self.size = (len(matrix), len(matrix[0]))
+
+    def get(self, line, column):
+        if not(type(line) == type(column) == 'int'):
+            print(f"Line and Column must be integers, line: {type(line)}, column: {type(column)}")
+        if self.scope == 'A':
+            return self.matrix[line][column]
+
+        elif self.scope == 'U':
+            if column > line:
+
+        elif self.scope == 'L':
+            
+
+    def valid(self):
+        
+
+
+
 A = np.array([[1,2,2]
              ,[4,4,2]
              ,[4,6,4]],float)
 
-
+# In: A -> Any matrix
+# Out: The scalar of the matrix A 
 def gauss_elimination(A):
     """Função que recebe uma matriz e retorna ela escalonada"""
 
@@ -35,12 +88,14 @@ def solve_equation(N = 0, ICOD = 1, IDET = 0, A = 0, B = 0, TolM = 0):
 
         return L , U
 
-    if (ICOD == 2): # Decomposição de Cholesky
+    elif (ICOD == 2): # Cholesky decomposition
         pass
-    if (ICOD == 3): # Método Jacobi
+    elif (ICOD == 3): # Jacobi Method
         pass
-    if (ICOD == 4): # Método Gauss-Seidel
+    elif (ICOD == 4): # Gauss-Seidel Method
         pass
 
+    else:
+        print("Invalid ICOD")
     return 0
 
