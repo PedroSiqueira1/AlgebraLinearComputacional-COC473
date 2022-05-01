7d0a2fc829f4796ddb67cf8d24c066aad955a9a8
ptk-trindade <PatrickTrindade@poli.ufrj.br>
2022-05-01 14:28:22 -0300

    forward_substitution testada e funcionando

--- operations.py
+++ operations.py
@@ -1,16 +1,14 @@
-﻿def forward_substitution(matrix_lu, vector_b):
+﻿def forward_substitution(matrix_l, vector_b):
     y = []
 
     for i in range(len(vector_b)):
         summ = 0
-        for j in range(i-1):
-            summ += matrix_lu[i][j] * y[j]
-
-        y[i] = (b[i] - summ)/matrix_lu[i][i]
+        for j in range(i):
+            summ += matrix_l[i][j] * y[j]
+         
+        y.append((vector_b[i] - summ)) #/matrix_l[i][i])
 
     return y
-
-
 
 def backward_substitution():
 
