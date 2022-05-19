86c552fb193c248f128aa9616e685e41700b20b1
ptk-trindade <PatrickTrindade@poli.ufrj.br>
2022-05-19 00:08:24 -0300

    gauss_elimination removida, não estava mais sendo usada

--- operations.py
+++ operations.py
@@ -37,21 +37,6 @@ def pivot(matrix_a, line):
     return matrix_a
 
 
-# In: matrix_a -> Any matrix
-# Out: The scalar of the matrix matrix_a 
-def gauss_elimination(matrix_a):
-    """Função que recebe uma matriz e retorna ela escalonada"""
-
-    for i in range(0, len(matrix_a)): 
-        for j in range(i+1, len(matrix_a)):
-            for k in range(i, len(matrix_a)): 
-                print((matrix_a[j][i] / matrix_a[i][i]), j,k)
-
-                matrix_a[j][k] = matrix_a[j][k] - (matrix_a[j][i] / matrix_a[i][i]) * matrix_a[i][k]
-                
-    return matrix_a
-
-
 # Jacobi
 
 def greater_jacobi(matrix):
@@ -120,20 +105,3 @@ def verify_symmetry(matrix):
                 # Not symmetric
                 return False
     return True
-
-def lu(matrix_a, matrix_b):
-    for i in range(0, len(matrix_a)):
-        for j in range(i+1, len(matrix_a)):
-            if matrix_a[i][i] == 0:
-                matrix_a = pivot(matrix_a, i)
-
-            matrix_a[j][i] = matrix_a[j][i] / matrix_a[i][i]
-        for k in range(i+1, len(matrix_a)):
-            for l in range(i+1, len(matrix_a)):
-                matrix_a[l][k] = matrix_a[l][k] - matrix_a[l][i] * matrix_a[i][k]
-    
-    matrix_b = forward_substitution(matrix_a, matrix_b, True)
-
-    matrix_b = backward_substitution(matrix_a, matrix_b)
-
-    return matrix_b
